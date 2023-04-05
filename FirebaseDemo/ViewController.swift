3734769e1b70a5b7da73501779333ee81eda6504
DhimanHarjot <harjotsingh1321998@gmail.com>
2023-04-05 11:34:36 -0400

    Initial commit

---
+++ FirebaseDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirebaseDemo
+//
+//  Created by Canadore Student on 2023-04-05.
+//  Copyright © 2023 Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
